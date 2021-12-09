c2de3c5ff24a19033b9c8c84639e3e5256203ebc
sophia <sh01806@srcs.org>
2021-12-09 15:41:08 -0800

    day 2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject SpikeBall;
     private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = transform.position - SpikeBall.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = SpikeBall.transform.position + offset;
     }
+
 }
